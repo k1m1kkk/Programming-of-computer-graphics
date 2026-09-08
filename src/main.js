4e3b70a9ae1a084fd2ba082814e4bb3ca67a82f0
Mikhail Kishkurno <kishkurnogit@gmail.com>
2026-09-08 14:10:18 +0300

    Delete old files

--- src/main.js
+++
@@ -1,6 +0,0 @@
-(function (root) {
-  const Lab = root.ColorLab;
-  const controller = new Lab.ColorController();
-  new Lab.ColorView(controller);
-  root.colorLabController = controller;
-})(globalThis);
--- test_windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-node --test tests\*.test.js
-pause
